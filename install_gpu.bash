862d8a52655043b10f347c590c098b69420c8022
Amshra267 <amanmishra267@gmail.com>
2022-03-21 23:33:43 +0530

    final changes maybe

--- install_gpu.bash
+++ install_gpu.bash
@@ -13,9 +13,6 @@ wget https://github.com/Amshra267/BOSCH_A-G_INTERIIT/releases/download/v1.0.0/ag
 wget https://github.com/Amshra267/BOSCH_A-G_INTERIIT/releases/download/v1.0.0/gender.h5 -p model_data/
 wget https://github.com/Amshra267/BOSCH_A-G_INTERIIT/releases/download/v1.0.0/unet.onnx -p model_data/
 wget https://github.com/Amshra267/BOSCH_A-G_INTERIIT/releases/download/v1.0.0/mars-small128.pb -p model_data/
-cd model_data
-unzip New_32CL_5LR_43Epoc.zip
-cd ..
 
 ## converting darknet weights to tensorflow model
 python3 save_model.py --model yolov4
