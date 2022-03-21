a2946a3bdffe19a4630f14c664e3722155e1e39b
Amshra267 <amanmishra267@gmail.com>
2022-03-21 23:40:46 +0530

    final changes

--- install_gpu.bash
+++ install_gpu.bash
@@ -9,10 +9,10 @@ wget https://github.com/AlexeyAB/darknet/releases/download/darknet_yolo_v3_optim
 
 ## making directory and adding one by one
 mkdir model_data
-wget https://github.com/Amshra267/BOSCH_A-G_INTERIIT/releases/download/v1.0.0/age.h5 -p model_data/
-wget https://github.com/Amshra267/BOSCH_A-G_INTERIIT/releases/download/v1.0.0/gender.h5 -p model_data/
-wget https://github.com/Amshra267/BOSCH_A-G_INTERIIT/releases/download/v1.0.0/unet.onnx -p model_data/
-wget https://github.com/Amshra267/BOSCH_A-G_INTERIIT/releases/download/v1.0.0/mars-small128.pb -p model_data/
+wget https://github.com/Amshra267/BOSCH_A-G_INTERIIT/releases/download/v1.0.0/age.h5 -P model_data/
+wget https://github.com/Amshra267/BOSCH_A-G_INTERIIT/releases/download/v1.0.0/gender.h5 -P model_data/
+wget https://github.com/Amshra267/BOSCH_A-G_INTERIIT/releases/download/v1.0.0/unet.onnx -P model_data/
+wget https://github.com/Amshra267/BOSCH_A-G_INTERIIT/releases/download/v1.0.0/mars-small128.pb -P model_data/
 
 ## converting darknet weights to tensorflow model
 python3 save_model.py --model yolov4
